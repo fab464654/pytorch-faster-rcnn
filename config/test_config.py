9b8bd21f1602fb6d36d43fcc4f636676c7e9ed98
fab464654 <76775232+fab464654@users.noreply.github.com>
2021-02-26 11:14:09 +0100

    Update test_config.py

--- config/test_config.py
+++ config/test_config.py
@@ -1,9 +1,9 @@
 class Config:
-    model_weights = " "
+    model_weights = "google_drive/MyDrive/ColabNotebooks/Project/_provaVanillaGitHub/faster_rcnn_1_6_10021.pth"
     image_path = " "
-    gpu_id = '2'
+    gpu_id = '0'
     num_classes = 80 + 1
-    data_root_dir = " "
+    data_root_dir = "google_drive/MyDrive/ColabNotebooks/Project/_provaVanillaGitHub/COCODevKit"
 
 
 test_cfg = Config()
